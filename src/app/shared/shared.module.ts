748c5780aadd86da6d75fdfce18ba55785c87273
Julia Gabellone <julia.a.gabellone@accenture.com>
2025-05-16 21:12:32 -0300

    Hasta 18

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { MediaPlayerComponent } from './components/media-player/media-player.component';
 import { HeaderUserComponent } from './components/header-user/header-user.component';
@@ -13,7 +14,11 @@ import { HeaderUserComponent } from './components/header-user/header-user.compon
     HeaderUserComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    SideBarComponent,
   ]
 })
 export class SharedModule { }
